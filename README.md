a341fd3b2a9b74bcff8613c128d722532f6d0651
helium-service <helium-service@chartboost.com>
2025-10-22 15:59:55 -0700

    [AUTO-GENERATED] Synchronizing changes from private repository

--- README.md
+++ README.md
@@ -12,7 +12,7 @@ In order to add the Chartboost Mediation Unity SDK - Vungle Adapter to your proj
 
 ```json
 "dependencies": {
-    "com.chartboost.mediation.unity.adapter.vungle": "5.0.3",
+    "com.chartboost.mediation.unity.adapter.vungle": "5.0.4",
     ...
 },
 "scopedRegistries": [
--- Runtime/Vungle/VungleAdapter.cs
+++ Runtime/Vungle/VungleAdapter.cs
@@ -12,7 +12,7 @@ namespace Chartboost.Mediation.Vungle
         /// <summary>
         /// The partner adapter Unity version.
         /// </summary>
-        public const string AdapterUnityVersion = "5.0.3";
+        public const string AdapterUnityVersion = "5.0.4";
         
         /// <inheritdoc cref="IPartnerAdapterConfiguration.AdapterNativeVersion"/>
         public static string AdapterNativeVersion => Instance.AdapterNativeVersion;
